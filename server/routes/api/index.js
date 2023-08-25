58fb9bb35580a0c43aefaa36a3e9544f49ab8f04
Tzuzu <natoiodice@gmail.com>
2023-08-24 20:20:08 -0700

    Added api routes and some controllers

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,11 +1,6 @@
 const router = require('express').Router();
-const path = require('path');
-const apiRoutes = require('./api');
+const userRoutes = require('./userRoutes');
 
-router.use('/api', apiRoutes);
-
-router.use((req, res) => {
-    res.sendFile(path.join(__dirname, '../../client/build/index.html'));
-})
+router.use('/users', userRoutes);
 
 module.exports = router;
