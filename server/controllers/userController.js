0b3a908ccdce8870253a2f6b2a516969fbb6ed0f
Gcreach <gcreach97@gmail.com>
2023-08-24 20:56:45 -0700

    models

---
+++ server/controllers/userController.js
@@ -0,0 +1,15 @@
+// TODO
+// Login User / Create user / Create Recipe / Load Recipe / Load all recipes
+
+const { user } = require('../models');
+const { signToken } = require('../utils/auth');
+
+module.exports = {
+    async createUser(req, res) {
+
+    },
+
+    async loginUser(req, res) {
+
+    }
+}
