d7532e332fa106c70c3991ec010ce33f8a4cb7cd
Tzuzu <natoiodice@gmail.com>
2023-08-28 20:13:29 -0700

    Added Krystal's typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,5 @@
 const { gql } = require('apollo-server-express');
+const { Recipe } = require('../models');
 
 const typeDefs = gql`
     type Recipe {
@@ -8,20 +9,34 @@ const typeDefs = gql`
         instructions: [String]
         rating: Float
     }
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+    }
     type Query {
         recipes: [Recipe]
         recipe(id: ID): Recipe
     }
 
     type Mutation {
+        createUser(username: String, email: String, password: String): User
+        loginUser(email: String, password: String): AuthPayload
         createRecipe(input: RecipeInput): Recipe
+        saveRecipe(userId: ID, recipeId: ID): Recipe
     }
 
     input RecipeInput {
         name: String
         ingredients: [String]
         instructions: [String]
+    }  
+    
+    type AuthPayload {
+        token: ID
+        user: User
     }
 `;
 
-module.exports = typeDefs
+module.exports = typeDefs;
