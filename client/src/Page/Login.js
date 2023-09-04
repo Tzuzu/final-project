b5e57dac7d4d9c4affcaf7b3a097b14ccbda7496
Krystal Richardson <krystal.williams2019@gmail.com>
2023-09-04 16:40:49 -0400

    need to check functionality

--- client/src/Page/Login.js
+++ client/src/Page/Login.js
@@ -1,25 +1,66 @@
-import React from 'react';
-import { useMutation } from '@apollo/client'
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { gql } from '@apollo/client';
 import './style.css';
 
+const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
 const Login = (props) => {
-    // const [formState, setFormState] = useState({ email: '', password: '' });
-    // const [login, { error, data }] = useMutation(LOGIN_USER);
+    const [formState, setFormState] = useState({ email: '', password: '' });
+    const [loginUser] = useMutation(LOGIN_USER);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const { data } = await loginUser({ 
+                variables: { 
+                    email: formState.email, 
+                    password: formState.password 
+                } 
+            });
+            console.log('Login successful:', data);
+        } catch (error) {
+            console.error('Login error:', error);
+        }
+    }
+
     return (
         <div className="login-container">
             <div className="login-box">
                 <div className="login-section">
                     <h2>Login</h2>
                     <p>Have an account? Sign in here</p>
-                    {/* <form onSubmit={ handleSubmit }> */}
-                    <form>
+                    <form onSubmit={ handleSubmit }>
                         <div>
                             <label htmlFor="email" className="form-label">Email</label>
-                                <input type="email" name="email" placeholder="Email"/>
+                                <input 
+                                type="email" 
+                                name="email" 
+                                placeholder="Email"
+                                value={ formState.email }
+                                onChange={(e) => setFormState({...formState, email: e.target.value })}
+                                />
                         </div>
                         <div>
                             <label htmlFor="password" className="form-label">Password</label>
-                                <input type="password" name="password" placeholder="Password"/>
+                                <input 
+                                type="password" 
+                                name="password" 
+                                placeholder="Password"
+                                value={ formState.password }
+                                onChange={(e) => setFormState({...formState, password: e.target.value })}
+                                />
                         </div>
                     </form>
                 </div>
@@ -29,11 +70,19 @@ const Login = (props) => {
                     <form>
                         <div>
                             <label htmlFor="email" className="form-label">Email</label>
-                                <input type="email" name="email" placeholder="Email"/>
+                                <input 
+                                type="email" 
+                                name="email" 
+                                placeholder="Email"
+                                />
                         </div>
                         <div>
                             <label htmlFor="password" className="form-label">Password</label>
-                                <input type="password" name="password" placeholder="Password"/>
+                                <input 
+                                type="password" 
+                                name="password" 
+                                placeholder="Password"
+                                />
                         </div>
                     </form>
                 </div>
