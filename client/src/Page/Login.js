c259854280e388278820c361bb0f411c3d8df1cd
Gcreach <gcreach97@gmail.com>
2023-09-04 16:05:36 -0700

    updated login

--- client/src/Page/Login.js
+++ client/src/Page/Login.js
@@ -1,25 +1,47 @@
 import React from 'react';
 import { useMutation } from '@apollo/client'
 import './style.css';
+import { LOGIN_USER } from './utils/mutations';
 
 const Login = (props) => {
-    // const [formState, setFormState] = useState({ email: '', password: '' });
-    // const [login, { error, data }] = useMutation(LOGIN_USER);
+     const [formState, setFormState] = useState({ email: '', password: '' });
+     const [login, { error, data }] = useMutation(LOGIN_USER);
+
+     const handleFormSubmit = async (event) => {
+     event.preventDefault();
+     try {
+        const mutationResponse = await login({
+                variables: { email: formState.email, password: formState.password },
+              });
+              const token = mutationResponse.data.login.token;
+              Auth.login(token);
+            } catch (e) {
+              console.log(e);
+            }
+    };
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormState({
+          ...formState,
+          [name]: value,
+        });
+      };
+
     return (
         <div className="login-container">
             <div className="login-box">
                 <div className="login-section">
                     <h2>Login</h2>
                     <p>Have an account? Sign in here</p>
-                    {/* <form onSubmit={ handleSubmit }> */}
-                    <form>
+                     <form onSubmit={ handleFormSubmit }> 
                         <div>
                             <label htmlFor="email" className="form-label">Email</label>
-                                <input type="email" name="email" placeholder="Email"/>
+                                <input type="email" name="email" placeholder="Email" onChange={handleChange}/>
                         </div>
                         <div>
                             <label htmlFor="password" className="form-label">Password</label>
-                                <input type="password" name="password" placeholder="Password"/>
+                                <input type="password" name="password" placeholder="Password" onChange={handleChange}/>
                         </div>
                     </form>
                 </div>
