f5bc300b56cb94cb61f7cf2dfc9dd427b2f29496
kevinverduzco27 <kevin.verduzco27@gmail.com>
2023-09-04 16:43:49 -0700

    fixed the issue with it not loading

--- client/src/Page/Login.js
+++ client/src/Page/Login.js
@@ -1,31 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import './style.css';
-import { LOGIN_USER } from './utils/mutations';
+import { LOGIN_USER } from './utils/mutations'; // Import your GraphQL mutation here
+import Auth from './utils/auth'; // Import your Auth component here
 
 const Login = (props) => {
-  // const [formState, setFormState] = useState({ email: '', password: '' });
-  // const [login, { error, data }] = useMutation(LOGIN_USER);
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error, data }] = useMutation(LOGIN_USER);
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const mutationResponse = await login({
+        variables: { email: formState.email, password: formState.password },
+      });
+      const token = mutationResponse.data.login.token;
+      Auth.login(token);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
   return (
     <div className="login-container">
       <div className="login-box">
         <div className="login-section">
           <h2>Login</h2>
           <p>Have an account? Sign in here</p>
-          {/* <form onSubmit={ handleSubmit }> */}
-          <form>
+          <form onSubmit={handleFormSubmit}>
             <div>
               <label htmlFor="email" className="form-label">
                 Email
               </label>
-              <input type="email" name="email" placeholder="Email" />
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                onChange={handleChange}
+              />
             </div>
             <div>
               <label htmlFor="password" className="form-label">
                 Password
               </label>
-              <input type="password" name="password" placeholder="Password" />
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                onChange={handleChange}
+              />
             </div>
+            <button type="submit">Login</button> {/* Added login button */}
           </form>
         </div>
         <div className="signup-section">
@@ -44,70 +77,12 @@ const Login = (props) => {
               </label>
               <input type="password" name="password" placeholder="Password" />
             </div>
+            <button type="submit">Sign Up</button> {/* Added sign-up button */}
           </form>
         </div>
       </div>
     </div>
   );
 };
-     const [formState, setFormState] = useState({ email: '', password: '' });
-     const [login, { error, data }] = useMutation(LOGIN_USER);
-
-     const handleFormSubmit = async (event) => {
-     event.preventDefault();
-     try {
-        const mutationResponse = await login({
-                variables: { email: formState.email, password: formState.password },
-              });
-              const token = mutationResponse.data.login.token;
-              Auth.login(token);
-            } catch (e) {
-              console.log(e);
-            }
-    };
-
-    const handleChange = (event) => {
-        const { name, value } = event.target;
-        setFormState({
-          ...formState,
-          [name]: value,
-        });
-      };
-
-    return (
-        <div className="login-container">
-            <div className="login-box">
-                <div className="login-section">
-                    <h2>Login</h2>
-                    <p>Have an account? Sign in here</p>
-                     <form onSubmit={ handleFormSubmit }> 
-                        <div>
-                            <label htmlFor="email" className="form-label">Email</label>
-                                <input type="email" name="email" placeholder="Email" onChange={handleChange}/>
-                        </div>
-                        <div>
-                            <label htmlFor="password" className="form-label">Password</label>
-                                <input type="password" name="password" placeholder="Password" onChange={handleChange}/>
-                        </div>
-                    </form>
-                </div>
-                <div className="signup-section">
-                    <h2>Sign-up</h2>
-                    <p>Not a user? Sign up here</p>
-                    <form>
-                        <div>
-                            <label htmlFor="email" className="form-label">Email</label>
-                                <input type="email" name="email" placeholder="Email"/>
-                        </div>
-                        <div>
-                            <label htmlFor="password" className="form-label">Password</label>
-                                <input type="password" name="password" placeholder="Password"/>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    );
-}
 
 export default Login;
