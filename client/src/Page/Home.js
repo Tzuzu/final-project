16f80b145e759c4f0a8d971053a17e6e53fda59e
kevinverduzco27 <kevin.verduzco27@gmail.com>
2023-09-04 12:16:48 -0700

    the startign css for the sections

--- client/src/Page/Home.js
+++ client/src/Page/Home.js
@@ -1,25 +1,39 @@
-import React from 'react'
+import React from 'react';
 import './style.css';
-
+import CategoryItem from './CategoryItem'; // Import the CategoryItem component
 
 const Home = () => {
-    return( 
+  return (
     <>
-    <div>
+      <div>
         <i />
-        <p> Welcome to everyone's cookbook! You may browse the site to find recipes you like, otherwise please checkout your cookbook page to create and manage your own!
+        <p>
+          Welcome to everyone's cookbook! You may browse the site to find
+          recipes you like, otherwise please check out your cookbook page to
+          create and manage your own!
         </p>
 
         <h2> Recipes </h2>
         <h3> What are you in the mood for? </h3>
-        <ul>
-            <li>Breakfast</li>
-            <li>Lunch</li>
-            <li>Dinner</li>
-            <li>Snacks</li>
-        </ul>
-    </div>
-    </>
-)}
 
-export default Home
+        <ul className="category-list">
+          {/* Render each category as a CategoryItem */}
+          <li>
+            <CategoryItem category="Breakfast" imageSrc="breakfast.jpg" />
+          </li>
+          <li>
+            <CategoryItem category="Lunch" imageSrc="lunch.jpg" />
+          </li>
+          <li>
+            <CategoryItem category="Dinner" imageSrc="dinner.jpg" />
+          </li>
+          <li>
+            <CategoryItem category="Snacks" imageSrc="snacks.jpg" />
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Home;
--- client/src/Page/Login.js
+++ client/src/Page/Login.js
@@ -1,45 +1,53 @@
 import React from 'react';
-import { useMutation } from '@apollo/client'
+import { useMutation } from '@apollo/client';
 import './style.css';
 
 const Login = (props) => {
-    // const [formState, setFormState] = useState({ email: '', password: '' });
-    // const [login, { error, data }] = useMutation(LOGIN_USER);
-    return (
-        <div className="login-container">
-            <div className="login-box">
-                <div className="login-section">
-                    <h2>Login</h2>
-                    <p>Have an account? Sign in here</p>
-                    {/* <form onSubmit={ handleSubmit }> */}
-                    <form>
-                        <div>
-                            <label htmlFor="email" className="form-label">Email</label>
-                                <input type="email" name="email" placeholder="Email"/>
-                        </div>
-                        <div>
-                            <label htmlFor="password" className="form-label">Password</label>
-                                <input type="password" name="password" placeholder="Password"/>
-                        </div>
-                    </form>
-                </div>
-                <div className="signup-section">
-                    <h2>Sign-up</h2>
-                    <p>Not a user? Sign up here</p>
-                    <form>
-                        <div>
-                            <label htmlFor="email" className="form-label">Email</label>
-                                <input type="email" name="email" placeholder="Email"/>
-                        </div>
-                        <div>
-                            <label htmlFor="password" className="form-label">Password</label>
-                                <input type="password" name="password" placeholder="Password"/>
-                        </div>
-                    </form>
-                </div>
+  // const [formState, setFormState] = useState({ email: '', password: '' });
+  // const [login, { error, data }] = useMutation(LOGIN_USER);
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <div className="login-section">
+          <h2>Login</h2>
+          <p>Have an account? Sign in here</p>
+          {/* <form onSubmit={ handleSubmit }> */}
+          <form>
+            <div>
+              <label htmlFor="email" className="form-label">
+                Email
+              </label>
+              <input type="email" name="email" placeholder="Email" />
             </div>
+            <div>
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input type="password" name="password" placeholder="Password" />
+            </div>
+          </form>
         </div>
-    );
-}
+        <div className="signup-section">
+          <h2>Sign-up</h2>
+          <p>Not a user? Sign up here</p>
+          <form>
+            <div>
+              <label htmlFor="email" className="form-label">
+                Email
+              </label>
+              <input type="email" name="email" placeholder="Email" />
+            </div>
+            <div>
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input type="password" name="password" placeholder="Password" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
