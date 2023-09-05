a475a487beaf3a9a6793df1a209461ce398dbe22
kevinverduzco27 <kevin.verduzco27@gmail.com>
2023-09-05 15:11:32 -0700

    fixed the url issue

--- client/src/Page/Home.js
+++ client/src/Page/Home.js
@@ -20,7 +20,7 @@ const Home = () => {
         <ul className="category-list">
           {/* Render each category as a CategoryItem */}
           <li>
-            <CategoryItem category="Breakfast" imageSrc="breakfast.jpg" />
+            <CategoryItem category="Breakfast" imageSrc="./images/breakfast.jepg" />
           </li>
           <li>
             <CategoryItem category="Lunch" imageSrc="lunch.jpg" />
